2531c5f8a70227da83a57dde25e3954e180308e5
WuHuaiSheng <zxc20044123@gmail.com>
2021-04-19 22:33:52 +0800

    Add new file

---
+++ driving.py
@@ -0,0 +1,17 @@
+country = input('請問你是哪國人(台灣或美國): ')
+if country == '台灣':
+	age = input('請輸入年齡: ')
+	age = int(age)
+	if age >= 18:
+		print('台灣人已達可以考駕照年齡')
+	else:
+		print('還不能考駕照')
+elif country == '美國':
+	age = input('請輸入年齡: ')
+	age = int(age)
+	if age >= 16:
+		print('美國人已達可以考駕照年齡')
+	else:
+		print('還不能考駕照')
+elif country != '台灣' and country != '美國':
+	print('輸入錯誤')
